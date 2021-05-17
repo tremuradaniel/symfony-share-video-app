23499c11f883ae1673813c1d78f1c08e6ed4c158
Daniel Tremura <tremuradaniel@gmail.com>
2021-05-17 17:12:46 +0300

    category and video entities

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $this->loadMainCategories($manager);
+    }
+
+    private function loadMainCategories($manager)
+    {
+        foreach ($this->getMainCategoriesData() as $name) {
+            $category = new Category();
+            $category->setName($name);
+            $manager->persist($category);
+
+        }
+
+        $manager->flush();
+
+    }
+
+    private function getMainCategoriesData()
+    {
+        return ['Electronics', 'Toys', 'Books', 'Movies'];
+    }
+}
