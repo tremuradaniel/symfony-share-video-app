545167905f383fba939ac56c8bc060042900daba
Daniel Tremura <tremuradaniel@gmail.com>
2021-05-28 09:43:05 +0300

    bat script to automate testing

---
+++ tests.bat
@@ -0,0 +1,22 @@
+rem run in cmd with: tests.bat
+
+@echo off
+if [%2]==[-db] goto rebuildDB
+if not [%1]==[] ( goto doConcreteTest) else ( goto doTest)
+goto :EOF
+ 
+:rebuildDB
+echo "rebuilding database ..."
+php bin/console doctrine:schema:drop -n -q --force --full-database
+del /f .\migrations\*.php
+php bin/console make:migration
+php bin/console doctrine:migrations:migrate -n -q
+php bin/console doctrine:fixtures:load -n -q
+ 
+:doTest
+php bin/phpunit
+goto :EOF
+ 
+:doConcreteTest
+php bin/phpunit %1
+goto :EOF
